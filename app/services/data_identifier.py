e9a7610262520ceae080e61476e1ed79612671f1
Krypton-KK <krypton.kk.a@proton.me>
2026-02-19 16:26:29 +0530

    fixed the unknown datatype error

--- app/services/data_identifier.py
+++ app/services/data_identifier.py
@@ -21,10 +21,10 @@ def identify_data_type(data: List[Dict]) -> str:
 def identify_data(param: str, vals: List[Customer | Analytics | Tickets]) -> str:
     if not param:
         return "empty"
-    if "date" == param:
+    if "date" in vals[0].model_dump().keys():
         return "time_series"
-    if ("ticket_id" == param or type(vals) == List[Tickets]):
+    if "ticket_id" in vals[0].model_dump().keys():
         return "tabular_support"
-    if ("customer_id" == param or type(vals) == List[Customer]):
+    if "customer_id" in vals[0].model_dump().keys():
         return "tabular_crm"
     return "unknown"
